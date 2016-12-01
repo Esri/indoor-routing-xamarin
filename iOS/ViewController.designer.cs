f10f3e29b1fef54e14baa186378ce47ef36d93ca
Mara Stoica <mstoica@esri.com>
2016-12-01 11:07:36 -0800

    UI Changes to add the home button to main screen

--- iOS/ViewController.designer.cs
+++ iOS/ViewController.designer.cs
@@ -19,22 +19,58 @@ namespace IndoorNavigation.iOS
 
         [Outlet]
         [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton CurrentLocation { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
         UIKit.UITableView FloorsTableView { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton Home { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton InvisibleButton { get; set; }
 
         [Outlet]
         [GeneratedCode ("iOS Designer", "1.0")]
         Esri.ArcGISRuntime.UI.Controls.MapView MapView { get; set; }
 
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton Settings { get; set; }
+
         void ReleaseDesignerOutlets ()
         {
+            if (CurrentLocation != null) {
+                CurrentLocation.Dispose ();
+                CurrentLocation = null;
+            }
+
             if (FloorsTableView != null) {
                 FloorsTableView.Dispose ();
                 FloorsTableView = null;
             }
 
+            if (Home != null) {
+                Home.Dispose ();
+                Home = null;
+            }
+
+            if (InvisibleButton != null) {
+                InvisibleButton.Dispose ();
+                InvisibleButton = null;
+            }
+
             if (MapView != null) {
                 MapView.Dispose ();
                 MapView = null;
+            }
+
+            if (Settings != null) {
+                Settings.Dispose ();
+                Settings = null;
             }
         }
     }
